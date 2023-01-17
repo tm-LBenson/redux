1a7c605a7904223e89f73f7e785f673482c23d35
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-17 12:22:48 -0500

    add placeholder img

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,9 @@
 /** @format */
 
-import { Card, Grid, ListItem } from '@mui/material';
+import { Card, CardMedia, Grid, ListItem } from '@mui/material';
 import React from 'react';
 import { connect } from 'react-redux';
+import image from '../images/placeholder.jpg';
 function Products({ products, activeCategory }) {
   const activeProducts = products.products.filter(
     (item) => item.category.toUpperCase() === activeCategory.activeCategory,
@@ -17,18 +18,30 @@ function Products({ products, activeCategory }) {
         <ul className="products">
           <Grid
             container
-            rowSpacing="1"
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            rowSpacing="30"
             columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
             {activeProducts.map((item, idx) => (
               <Grid
+                width="100"
                 key={idx}
                 item
                 xs={6}
               >
-                <Card variant="outlined">
-                  <ListItem>{item.name}</ListItem>
-                </Card>
+                <div className="products__items">
+                  <Card variant="outlined">
+                    <CardMedia
+                      component="img"
+                      alt="item.name"
+                      height="180"
+                      image={image}
+                    />
+                    <ListItem>{item.name}</ListItem>
+                  </Card>
+                </div>
               </Grid>
             ))}
           </Grid>
