cba990c66a37821567b575c78d9864513e3a1d34
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-18 22:09:53 -0500

    update tests

--- src/components/__tests__/Products.test.jsx
+++ src/components/__tests__/Products.test.jsx
@@ -1,14 +1,28 @@
 /** @format */
 
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from '../../store/store';
 import Products from '../Products';
+import { Provider } from 'react-redux';
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
+import productsReducer from '../../store/productsReducer';
+import activeCategoriesReducer from '../../store/activeCategoryReducer';
+import { render } from '@testing-library/react';
+import thunk from 'redux-thunk';
 
-describe('Products component', () => {
-  test('Should render the component without crashing', () => {
+describe('Products', () => {
+  const reducers = combineReducers({
+    products: productsReducer,
+    activeCategory: activeCategoriesReducer,
+  });
+
+  const store = createStore(reducers, applyMiddleware(thunk));
+
+  test('renders without crashing', () => {
     render(
-      <Provider store={store()}>
+      <Provider store={store}>
         <Products />
       </Provider>,
     );
